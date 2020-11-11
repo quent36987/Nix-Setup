4226d4f1a286258085b82db4a68336433da26ea5
Valentin Chassignol <Vinetosdev@gmail.com>
2020-11-11 14:48:23 +0100

    feat: Add video resolution

--- i3.nix
+++ i3.nix
@@ -48,6 +48,11 @@ in
         always = true;
         notification = false;
       }
+
+      {
+        command = "${pkgs.bash}/bin/bash xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x309 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal";
+        always = true;
+      }
     ];
 
   };
